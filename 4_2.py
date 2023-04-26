cb2bda5abea282bc498d7221e6f6d4497c05907d
Vitalik2407 <vitalik240791@mail.ru>
2023-04-26 19:30:02 +0300

    add 4_3

--- 4_2.py
+++ 4_2.py
@@ -1,10 +1,11 @@
 # Без использования collections, написать программу,
 # которая будет создавать словарь для подсчитывания
 # количества вхождений каждой буквы в текст введенный с клавиатуры.
+
 text = input('Введите текст:')
 
-mn_text = set(text)
-list_text = list(mn_text)
+list_text = list(text)
 
 ftext = {list_text[i]: text.count(list_text[i]) for i in range(len(list_text))}
 print(ftext)
+print(list_text)
